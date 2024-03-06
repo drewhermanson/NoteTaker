ae672c03c03883f0af48ea5cb151bacd24b9b1b2
drewhermanson <97925194+drewhermanson@users.noreply.github.com>
2024-03-05 21:14:43 -0600

    added deleting of comments and made notes.js code variables match index.js. also added comments where needed

--- server.js
+++ server.js
@@ -18,12 +18,12 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// GET Route for feedback page
+// GET Route for notes page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-// Wildcard route to direct users the index
+// Wildcard route to redirect users to the index
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/index.html'))
 );
